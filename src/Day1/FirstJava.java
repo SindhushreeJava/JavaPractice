53a36c90745bbf09e35b1c192ef845ab25f03f61
Sindhushree <sindhushree.hu@pivotree.com>
2025-05-18 18:23:15 +0530

    AddedPackage

--- src/Day1/FirstJava.java
+++ src/Day1/FirstJava.java
@@ -1,3 +1,5 @@
+package Day1;
+
 public class FirstJava {
     public static void main(String args[])
     {
--- src/Day1/MySecond.java
+++ src/Day1/MySecond.java
@@ -1,3 +1,5 @@
+package Day1;
+
 public class MySecond {
     public static void main(String[] args) {
         System.out.println("Executing the second class");
